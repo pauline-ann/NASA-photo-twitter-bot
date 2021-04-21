4f16d24eb0750b2e04533a874c1c89e1f5850c14
pauline-ann <paulinebantayan@gmail.com>
2021-04-21 13:17:43 -0700

    implement async functions to gather data before posting tweet

--- bot.js
+++ bot.js
@@ -17,69 +17,75 @@ let NASA_API = 'https://api.nasa.gov/planetary/apod?api_key=' + NASA_API_KEY;
 
 // -------------------------------------
 
-function tweet() {
-
-    // Gather data from NASA API
-    axios.get(NASA_API)
-        .then((res) => {
-            // handle success   
-
-            // let status = res.status;
-            // let date = res.data.date;
-            // Shorten explanation to 1 or 2 sentences
-            // let explanation = res.data.explanation;
-            let title = res.data.title;
-            let hdURL = res.data.hdurl;
-            let imageURL = res.data.url;
-            let base64str;
-
-            // Convert image into encoded base 64
-            // imageToBase64(imageURL)
-            //     .then(
-            //         (response) => {
-            //             base64str = response;
-            //         }
-            //     )
-            //     .catch(
-            //         (error) => {
-            //             console.log(error); // Logs an error if there was one
-            //         }
-            //     )
-
-            console.log('Uploading image...')
-
-            // Make this an asynchronous function
-            // Post daily tweet image
-            T.post('media/upload', { media_data: base64str }, function (err, data, response) {
-                if (err) {
-                    console.log('Something went wrong!')
-                    console.log(err)
-                } else {
-                    console.log('Success! Image uploaded. Now tweeting...')
-
-                    T.post('statuses/update', {
-                        media_ids: new Array(data.media_id_string),
-                        status: `💫${title}\n${hdURL}\n#NASA #Space`
-                    },
-                        function (err, data, response) {
-                            if (err) {
-                                console.log('error:', err);
-                            }
-                            else {
-                                console.log('Tweeted an image!');
-                            }
-                        }
-                    );
+const encodeToBase64 = (imageURL) => {
+    return new Promise(resolve => {
+        // Convert image into encoded base 64
+        imageToBase64(imageURL)
+            .then(
+                (response) => {
+                    resolve(response)
                 }
-            })
-        })
-        .catch((err) => {
-            // handle error
-            console.log(err);
-        });
+            )
+            .catch(
+                (error) => {
+                    // Logs an error if there was one
+                    console.log(error);
+                }
+            )
+    })
 }
 
-tweet();
+const getData = () => {
+    return new Promise(resolve => {
+        // Gather data from NASA API
+        axios.get(NASA_API)
+            .then(async (res) => {
+                // handle success 
+                resolve(res)
+            })
+            .catch((err) => {
+                // handle error
+                console.log(err);
+            });
+    })
+}
+
+const tweet = async () => {
+
+    const res = await getData();
+
+    const imageURL = res.data.url;
+    const title = res.data.title;
+    const hdURL = res.data.hdurl;
+
+    const base64str = await encodeToBase64(imageURL)
+
+    console.log('Uploading image...')
+
+    // Post daily tweet image
+    T.post('media/upload', { media_data: base64str }, function (err, data, response) {
+        if (err) {
+            console.log('Something went wrong!')
+            console.log(err)
+        } else {
+            console.log('Success! Image uploaded. Now tweeting...')
+
+            T.post('statuses/update', {
+                media_ids: new Array(data.media_id_string),
+                status: `${title}\n${hdURL}\n#NASA #Space #Science`
+            },
+                function (err, data, response) {
+                    if (err) {
+                        console.log('error:', err);
+                    }
+                    else {
+                        console.log('Tweeted an image!');
+                    }
+                }
+            );
+        }
+    })
+}
 
 // Run every 24 hours
-// setInterval(function () { tweet(); }, 216000000)
+setInterval(function () { tweet(); }, 216000000)
