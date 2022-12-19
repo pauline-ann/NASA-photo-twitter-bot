6d46ce15332a9555f595d54dd2a291b5aac39ebd
pauline-ann <paulinebantayan@gmail.com>
2022-12-18 20:32:37 -0800

    reformat

--- bot.js
+++ bot.js
@@ -1,19 +1,19 @@
 //------ BOT SET-UP -------//
 
-require("dotenv").config();
-const twit = require("twit");
-const axios = require("axios");
-const imageToBase64 = require("image-to-base64");
+require("dotenv").config()
+const twit = require("twit")
+const axios = require("axios")
+const imageToBase64 = require("image-to-base64")
 
 const T = new twit({
   consumer_key: process.env.API_KEY,
   consumer_secret: process.env.API_SECRET_KEY,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-});
+})
 
-let NASA_API_KEY = process.env.NASA_API_KEY;
-let NASA_API = "https://api.nasa.gov/planetary/apod?api_key=" + NASA_API_KEY;
+let NASA_API_KEY = process.env.NASA_API_KEY
+let NASA_API = "https://api.nasa.gov/planetary/apod?api_key=" + NASA_API_KEY
 
 // -------------------------------------
 
@@ -22,14 +22,14 @@ const encodeToBase64 = (imageURL) => {
     // Convert image into encoded base 64
     imageToBase64(imageURL)
       .then((response) => {
-        resolve(response);
+        resolve(response)
       })
       .catch((error) => {
         // Logs an error if there was one
-        console.log(error);
-      });
-  });
-};
+        console.log(error)
+      })
+  })
+}
 
 const getData = () => {
   return new Promise((resolve) => {
@@ -38,32 +38,32 @@ const getData = () => {
       .get(NASA_API)
       .then(async (res) => {
         // handle success
-        resolve(res);
+        resolve(res)
       })
       .catch((err) => {
         // handle error
-        console.log(err);
-      });
-  });
-};
+        console.log(err)
+      })
+  })
+}
 
 const tweet = async () => {
-  const res = await getData();
-  const title = res.data.title;
-  const mediaType = res.data.media_type;
+  const res = await getData()
+  const title = res.data.title
+  const mediaType = res.data.media_type
   console.log(mediaType)
 
   // Post is a video
   if (mediaType === "video") {
-    let videoURL = res.data.url;
+    let videoURL = res.data.url
 
     // Adjust YouTube URL so that the preview displays on Twitter
     // Replace part of the URL
     videoURL = videoURL.replace('embed/', 'watch?v=')
     // Take out last 6 characters of API url: '?rel=0'
-    videoURL = videoURL.substring(0, videoURL.length - 6);
+    videoURL = videoURL.substring(0, videoURL.length - 6)
 
-    console.log("Uploading video link...");
+    console.log("Uploading video link...")
 
     T.post(
       "statuses/update",
@@ -72,20 +72,20 @@ const tweet = async () => {
       },
       function (err, data, response) {
         if (err) {
-          console.log("error:", err);
+          console.log("error:", err)
         } else {
-          console.log("Tweeted a video link!");
+          console.log("Tweeted a video link!")
         }
       }
-    );
+    )
   }
   // Post is an image
   else {
-    const imageURL = res.data.url;
-    const hdURL = res.data.hdurl;
-    const base64str = await encodeToBase64(imageURL);
+    const imageURL = res.data.url
+    const hdURL = res.data.hdurl
+    const base64str = await encodeToBase64(imageURL)
 
-    console.log("Uploading image...");
+    console.log("Uploading image...")
 
     // Post daily tweet image
     T.post(
@@ -93,10 +93,10 @@ const tweet = async () => {
       { media_data: base64str },
       function (err, data, response) {
         if (err) {
-          console.log("Something went wrong!");
-          console.log(err);
+          console.log("Something went wrong!")
+          console.log(err)
         } else {
-          console.log("Success! Image uploaded. Now tweeting...");
+          console.log("Success! Image uploaded. Now tweeting...")
 
           T.post(
             "statuses/update",
@@ -106,17 +106,17 @@ const tweet = async () => {
             },
             function (err, data, response) {
               if (err) {
-                console.log("error:", err);
+                console.log("error:", err)
               } else {
-                console.log("Tweeted an image!");
+                console.log("Tweeted an image!")
               }
             }
-          );
+          )
         }
       }
-    );
+    )
   }
-};
+}
 
 // Run every 24 hours
-setInterval(function () { tweet(); }, 86400000);
+setInterval(() => { tweet() }, 86400000)
